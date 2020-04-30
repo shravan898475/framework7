fff7ee8b392f9128550cb9343a073ae8a958919b
shravan898475 <cshravansingh45@gmail.com>
2020-04-30 12:01:25 +0530

    add custom page

---
+++ kitchen-sink/core/js/app.js
@@ -0,0 +1,35 @@
+// Dom7
+var $ = Dom7;
+
+// Theme
+var theme = 'auto';
+if (document.location.search.indexOf('theme=') >= 0) {
+  theme = document.location.search.split('theme=')[1].split('&')[0];
+}
+
+// Init App
+var app = new Framework7({
+  id: 'io.framework7.testapp',
+  root: '#app',
+  theme: theme,
+  data: function () {},
+  methods: {},
+
+  routes: routes,
+  popup: {
+    closeOnEscape: true,
+  },
+  sheet: {
+    closeOnEscape: true,
+  },
+  popover: {
+    closeOnEscape: true,
+  },
+  actions: {
+    closeOnEscape: true,
+  },
+  
+});
+
+var mainView = app.views.create('.view-main');
+
